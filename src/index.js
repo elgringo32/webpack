f0204086843550b1aedcf5ee33ed8f48b24729bf
elgringo32 <bknopke@gmail.com>
2021-02-03 22:29:11 -0800

    added logo, hero img, hero heading

--- src/index.js
+++ src/index.js
@@ -5,10 +5,21 @@ const headingSection = document.createElement('div');
 contentSection.appendChild(headingSection);
 headingSection.setAttribute('id','heading-section');
 
+const logoSection = document.createElement('img');
+logoSection.setAttribute('src','../img/canva-black-with-utensils-icon-restaurant-logo-cKWpOK2DXM4.jpg');
+headingSection.appendChild(logoSection);
+
 const heroSection = document.createElement('div');
 contentSection.appendChild(heroSection);
 heroSection.setAttribute('id','hero-section');
 
+const heroSectionHeading = document.createElement('h1');
+heroSection.appendChild(heroSectionHeading);
+heroSectionHeading.setAttribute('id','hero-section-heading');
+heroSectionHeading.innerText = "Come Eat Amazing Food in a Relaxed Environment";
+
+
 const bodySection = document.createElement('div');
 contentSection.appendChild(bodySection);
 bodySection.setAttribute('id','body-section');
+
