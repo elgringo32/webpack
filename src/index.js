5aa8120f93ad6f45282eeade78af71b697ae07e1
elgringo32 <bknopke@gmail.com>
2021-02-08 23:52:12 -0800

    added tabs starter code

--- src/index.js
+++ src/index.js
@@ -23,3 +23,54 @@ const bodySection = document.createElement('div');
 contentSection.appendChild(bodySection);
 bodySection.setAttribute('id','body-section');
 
+const tabWrapper = document.createElement('div');
+tabWrapper.setAttribute('class','wrapper');
+bodySection.appendChild(tabWrapper);
+
+const numToWords = ['one','two','three'];
+for (let i = 0; i < 3; i++) {
+    const radioInput = document.createElement('input');
+    radioInput.setAttribute('class','radio');
+    radioInput.setAttribute('id',numToWords[i]);
+    radioInput.setAttribute('name','group');
+    radioInput.setAttribute('type','radio');
+    if (i===0) {
+        radioInput.setAttribute('checked','');
+    }
+    tabWrapper.appendChild(radioInput);
+}
+
+const tabsClass = document.createElement('div');
+tabsClass.setAttribute('class','tabs');
+tabWrapper.appendChild(tabsClass);
+
+for (let i = 0; i < 3; i++) {
+    const tabClass = document.createElement('label');
+    tabClass.setAttribute('class','tab');
+    tabClass.setAttribute('id',`${numToWords[i]}-tab`);
+    tabClass.setAttribute('for',numToWords[i]);
+    tabClass.innerText = 'Find Us'
+    tabsClass.appendChild(tabClass);
+}
+
+
+const panelsClass = document.createElement('div');
+panelsClass.setAttribute('class','panels');
+tabWrapper.appendChild(panelsClass);
+
+for (let i = 0; i < 3; i++) {
+    const panelClass = document.createElement('div');
+    panelClass.setAttribute('class','panel');
+    panelClass.setAttribute('id',`${numToWords[i]}-panel`);
+    const panelTitle = document.createElement('div');
+    panelTitle.setAttribute('class','panel-title');
+    panelTitle.innerText = 'Find Us'
+    panelClass.appendChild(panelTitle);
+    const panelBody = document.createElement('p');
+    panelTitle.setAttribute('class','panel-title');
+    panelBody.innerText = 'Without CSS, every web page would be drab plain text and images that flowed straight down the page. With CSS, you can add color and background images and change the layout of your page — your web pages can feel like works of art!'
+    panelClass.appendChild(panelTitle);
+    panelClass.appendChild(panelBody);
+    panelsClass.appendChild(panelClass);
+}
+
